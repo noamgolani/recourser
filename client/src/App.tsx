9e89a3f612149501c4980a13b8a5515dcc8e2327
Noam Golani <noam.golani@gmail.com>
2022-01-07 00:43:40 +0200

    starting the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+	return (
+		<div className="page">
+			<div className="container"></div>
+		</div>
+	);
+}
+
+export default HomePage;
