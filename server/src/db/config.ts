f8d09e478ad1bda9716037f8f71f0ba6caf9b5a3
Noam Golani <noam.golani@gmail.com>
2022-01-10 12:15:46 +0200

    db connection git add .

--- server/src/db/config.ts
+++ server/src/db/config.ts
@@ -3,10 +3,12 @@ import { Dialect, Sequelize } from "sequelize";
 const dbName = process.env.DB_NAME as string;
 const dbUser = process.env.DB_USER as string;
 const dbHost = process.env.DB_HOST;
+const dbPort = +(process.env.DB_PORT || 5432);
 const dbDriver = process.env.DB_DRIVER as Dialect;
 const dbPassword = process.env.DB_PASSWORD;
 
 const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
+	port: dbPort,
 	host: dbHost,
 	dialect: dbDriver,
 });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,16 @@
-import app from './api/app';
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./api/app";
+
+import initDB from "./db/init";
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, ()=>{
-	console.log(`Listening on port: ${port}`);
-})
+const start = async () => {
+	await initDB();
+	app.listen(port, () => {
+		console.log(`Listening on port: ${port}`);
+	});
+};
+
+start();
