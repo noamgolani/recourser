459ba2b2618cc48195d09de3112fe02817f88f6e
Noam Golani <noam.golani@gmail.com>
2022-01-10 11:10:36 +0200

    db work

---
+++ server/src/db/models/User.ts
@@ -0,0 +1,53 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelizeConnection from "../config";
+
+interface UserAttributes {
+	id: number;
+	password: string;
+	username: string;
+	email: string;
+	createdAt?: Date;
+	updatedAt?: Date;
+	deletedAt?: Date;
+}
+
+export interface UserInput extends Optional<UserAttributes, "id"> {}
+export interface UserOutput extends Required<UserAttributes> {}
+
+class User extends Model<UserAttributes, UserInput> implements UserAttributes {
+	public id!: number;
+	public password!: string;
+	public username!: string;
+	public email!: string;
+
+	public readonly createdAt!: Date;
+	public readonly updatedAt!: Date;
+	public readonly deletedAt!: Date;
+}
+
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER.UNSIGNED,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+	},
+	{ timestamps: true, sequelize: sequelizeConnection, paranoid: true }
+);
+
+export default User;
