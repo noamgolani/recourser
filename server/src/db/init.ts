b401849ef9df5e87668c1e17f2171287429f5142
Noam Golani <noam.golani@gmail.com>
2022-01-11 11:50:25 +0200

    Recourse model

--- server/src/db/init.ts
+++ server/src/db/init.ts
@@ -1,9 +1,11 @@
-import User from './models/User';
+import User from "./models/User";
+import { Recourse } from "./models/Recourse";
 
 const isDev = process.env.NODE_ENV === "development";
 
 const dbInit = () => {
 	User.sync({ alter: isDev });
+	Recourse.sync({ alter: isDev });
 };
 
 export default dbInit;
